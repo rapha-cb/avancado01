d2a825cf681785ffd122496be41332e5bc355b02
raphael cordeiro <raphaelcbpereira@gmial.com>
2025-04-30 17:20:53 -0300

    exercicios listas

---
+++ biblioteca.py
@@ -0,0 +1,51 @@
+def imprime_nome(nome):
+    print(f"Nome: {nome}")
+
+def piramide(n):
+    for i in range(1,n+1):
+        for j in range(0, i):
+            print(i, end= " ")
+        print()
+
+def vogais(texto):
+    contagem = 0
+    for i in range(0, len(texto)):
+        if texto[i] == "a" or texto[i] =="e" or texto[i] == "i" or texto[i] =="o" or texto[i] =="u":
+            contagem+= 1
+    print(contagem)
+def valor_estoque(produto, qntd, valor):
+    valor_total= qntd* valor
+    return valor_total
+
+def caractere(numero):
+    if numero > 0:
+        return 'P'
+    elif numero < 0:
+        return 'N'
+    else:
+        return 'Z'
+
+def soma(*a):
+    somar= 0
+    for x in range(len(a)):
+        somar += a[x]
+    print(somar)
+
+def texto_principal(texto, ):
+    cont = 0
+    for i in range(len(texto)):
+        if texto[i] != " ":
+            cont+= 1
+    print(f"quantidade de letras: {cont}")
+    for j in range(len(texto)-1,-1,-1):
+        print(texto[j], end=" ")
+
+def verifica_lista(lista):
+    nova_lista= []
+    for i in range(len(lista)):
+        if nova_lista[i]!= lista[i]:
+            a= nova_lista.append
+
+
+
+
---
+++ ex01.py
@@ -0,0 +1,17 @@
+from biblioteca import *
+#piramide(9)
+#texto= "o rato roeu a roupa do rei de Roma"
+#vogais(texto)
+"""resp= valor_estoque("pirulito",50,0.5)
+print(f"valor total do estoque é: {resp}")"""
+
+"""resp= caractere(0)
+print(resp)"""
+
+#soma(8,7,9,10,1)
+#soma(8,7)
+#texto = "meu nome e julia"
+#texto_principal(texto)
+lista=[10,12,12,31,4,4,5,31,6,7,6,8]
+verifica_lista(lista)
+print()
